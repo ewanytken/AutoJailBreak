d8b9de1a82de912bafdd17fa6e4e9bf6774035b6
ewanytken <ewanytken@mail.ru>
2025-07-25 21:51:22 +0300

    repo test

---
+++ app/repository/jailbreak_controller_db.py
@@ -0,0 +1,65 @@
+from pathlib import Path
+
+import uvicorn
+import yaml
+from fastapi import FastAPI, Body, Request
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+from app.core import ScenarioFacade
+from app.repository import DatabaseInterface, DatabaseService
+
+
+class JailBreakControllerDB:
+
+    def __init__(self, db_connection: DatabaseInterface):
+        self.app = FastAPI()
+        self.register_request()
+
+        self.templates = Jinja2Templates(directory="templates")
+        self.app.mount("/static", StaticFiles(directory="templates/static"), name="static")
+
+        self.app.add_middleware(
+            CORSMiddleware,
+            allow_origins=["*"],
+            allow_credentials=True,
+            allow_methods=["*"],
+            allow_headers=["*"],
+        )
+
+        address_path = Path(__file__).parent.parent.parent / 'config.yaml'
+        with open(address_path, 'r') as file:
+            address = yaml.safe_load(file)
+
+        self.host = address['address']['host']
+        self.port = address['address']['port']
+
+        self.db_connection = db_connection
+        self.db_service = DatabaseService(db_connection)
+
+    def register_request(self):
+
+        @self.app.post('/autojailbreak', status_code=200)
+        def auto_jailbreak(json = Body()):
+            scenario = ScenarioFacade(json)
+            return {"result": scenario.get_dialog()}
+            # return {"result": "{}".format(scenario.get_dialog())} string format for JS
+
+        @self.app.get('/info', status_code=200)
+        async def info():
+            return {"version": "AutoJailBreak Service version: 0.0.1"}
+
+        @self.app.get("/index", response_class=HTMLResponse)
+        async def index_page(request: Request):
+            return self.templates.TemplateResponse("index.html", {"request": request})
+
+        @self.app.post('/test', status_code=200)
+        def auto_jailbreak(json = Body()):
+            self.db_service.add_dialog("{}".format(json))
+
+            return {"result": json}
+
+    def start(self):
+        uvicorn.run(self.app, host=self.host, port=self.port, log_level="info")
