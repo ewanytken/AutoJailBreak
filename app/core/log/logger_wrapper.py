31b380de68cce99f4426b6ed130267ff2a9a4595
ewanytken <ewanytken@mail.ru>
2025-09-03 23:20:14 +0300

    fix logger

--- app/core/log/logger_wrapper.py
+++ app/core/log/logger_wrapper.py
@@ -3,21 +3,24 @@ import logging
 class LoggerWrapper:
 
     def __init__(self, level=logging.INFO):
-
         self.logger = logging.getLogger(str(__package__))
-        self.logger.setLevel(level)
-        self.level = level
 
-        handlerFile = logging.FileHandler(str(__package__), mode='w')
-        handlerConsole = logging.StreamHandler()
+        if not self.logger.handlers:
+            self.logger.setLevel(level)
+            self.level = level
 
-        formatter = logging.Formatter("%(asctime)s %(name)s %(levelname)s %(message)s")
+            handlerFile = logging.FileHandler(str(__package__), mode='w')
+            handlerConsole = logging.StreamHandler()
 
-        handlerFile.setFormatter(formatter)
-        handlerConsole.setFormatter(formatter)
+            formatter = logging.Formatter("%(asctime)s %(name)s %(levelname)s %(message)s")
 
-        self.logger.addHandler(handlerFile)
-        self.logger.addHandler(handlerConsole)
+            handlerFile.setFormatter(formatter)
+            handlerConsole.setFormatter(formatter)
+
+            self.logger.addHandler(handlerFile)
+            self.logger.addHandler(handlerConsole)
+        else:
+            self.level = level
 
     def __call__(self, message):
         self.logger.log(self.level, message)
