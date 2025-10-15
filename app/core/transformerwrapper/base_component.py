fix memory calculation with pynvml

app/core/transformerwrapper/base_component.py
@@ -4,6 +4,7 @@ from abc import abstractmethod
 from typing import Union, Any, Dict, Optional
 
 import psutil
+import pynvml
 import torch
 from huggingface_hub import snapshot_download
 from torch import nn
@@ -41,15 +42,21 @@ class BaseComponent(ABC):
         raise NotImplemented
 
     def set_gpu_distribution(self) -> None:
+
         if torch.cuda.is_available() and self.use_cpu_only is False:
             try:
+                pynvml.nvmlInit()
                 for gpu_id in range(torch.cuda.device_count()):
+                    handle = pynvml.nvmlDeviceGetHandleByIndex(gpu_id)
+                    info = pynvml.nvmlDeviceGetMemoryInfo(handle)
                     vram_not_allocated = torch.cuda.get_device_properties(gpu_id).total_memory - torch.cuda.memory_allocated(gpu_id)
                     vram_not_reserved = torch.cuda.get_device_properties(gpu_id).total_memory - torch.cuda.memory.memory_reserved(gpu_id)
-                    if vram_not_allocated > self.get_model_size() * 1.03 and vram_not_reserved > self.get_model_size() * 1.03: # +3% size
+                    vram_not_placed = info.free # OVERALL FREE MEMORY INCLUDE OTHER APP
+                    if (vram_not_allocated > self.get_model_size() * 1.03 and vram_not_reserved > self.get_model_size() * 1.03
+                            and vram_not_placed > self.get_model_size() * 1.03): # +3% size
                         log(f"Current CUDA: {gpu_id}")
                         log(f"Model Size: {self.get_model_size()}")
-                        log(f"Free VRAM: {vram_not_allocated}")
+                        log(f"Free VRAM: {vram_not_placed}")
                         self.device = "cuda:{}".format(gpu_id)
                         self.model.to(self.device)
                         break
@@ -58,6 +65,8 @@ class BaseComponent(ABC):
                     self.model = self.multi_gpu()
             except:
                 raise GPUFindError
+            finally:
+                pynvml.nvmlShutdown()
 
         else:
             self.device = "cpu"
@@ -103,22 +112,28 @@ class BaseComponent(ABC):
 
         if torch.cuda.is_available():
             try:
+                pynvml.nvmlInit()
+
                 for gpu_id in range(torch.cuda.device_count()):
-                    memory_available = (torch.cuda.get_device_properties(gpu_id).total_memory / 1024 ** 3 -
-                                        torch.cuda.memory_allocated(gpu_id) / 1024 ** 3)
+                    handle = pynvml.nvmlDeviceGetHandleByIndex(gpu_id)
+                    info = pynvml.nvmlDeviceGetMemoryInfo(handle)
+
+                    memory_available = info.free / (1024 ** 2)
 
                     memory_allocated_overall += memory_available
                     memory_distribution.update({gpu_id: "{}GiB".format(math.floor(memory_available))})
 
             except Exception as err:
                 log(err)
+            finally:
+                pynvml.nvmlShutdown()
 
         if memory_allocated_overall < model_size:
             memory_distribution.update({"cpu": "{}GiB".format(math.floor(self.get_free_ram()))})
         log(memory_distribution)
         return memory_distribution
 
-    def get_model_size(self) -> float:
+    def get_model_size(self) -> int:
         param_size = 0
         for param in self.model.parameters():
             param_size += param.nelement() * param.element_size()